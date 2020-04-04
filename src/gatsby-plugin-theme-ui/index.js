07eb4a6b83292ed5e18a17671170625f5e9cd690
seandillingham <seandillingham@signalsciences.com>
2020-04-03 20:35:43 -0700

    add theme object primitives [closes #4]

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,8 +1,62 @@
 export default {
   colors: {
-    text: "#D8D7E0",
-    background: "#161533",
-    primary: "#639",
-    secondary: "#ff6347",
+    text: '#D8D7E0',
+    background: '#161533',
+    han: '#3F39C1',
+    slates: [
+      '#A7A3FF',
+      '#8A85FF',
+      '#655EFF',
+      '#161533',
+    ],
+    grays: [
+      '#FFFFFF',
+      '#F3F2F7',
+      '#D8D7E0',
+      '#9F9EAE',
+      '#6F6E87',
+      '#4A495D',
+      '#131229',
+      '#10101E',
+      '#09090C',
+    ],
   },
+  fonts: {
+    mono: 'apercu-mono-pro',
+    sans: 'apercu-pro, sans-serif',
+    serif: 'canela, serif',
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    bold: 700,
+  },
+  fontSizes: [
+    12, 15, 17, 19, 22, 27, 32, 37, 42, 50, 59
+  ],
+  letterSpacings: {
+    compact: '-0.02em',
+    standard: '0',
+    comfortable: '0.05em',
+    wide: '0.10em',
+    extrawide: '0.15em',
+  },
+  lineHeights: {
+    tight: 1.2,
+    compact: 1.25,
+    standard: 1.47,
+    comfortable: 1.6,
+    generous: 2,
+  },
+  space: [
+  0, 7, 14, 28, 35, 56, 84, 112
+  ],
+  breakpoints: [
+    '768px', '1025px', '1356px'
+  ],
+  mediaQueries: {
+    small: '@media screen and (min-width: ${breakpoints[0]})',
+    medium: '@media screen and (min-width: ${breakpoints[1]})',
+    large: '@media screen and (min-width: ${breakpoints[2]})',
+  }
 }
