9c081f76ecb049abd5606bc627c2f4e884ce1625
seandillingham <seandillingham@signalsciences.com>
2020-04-15 18:38:40 -0700

    add robots.txt [closes #46]

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,7 @@ module.exports = {
     },
     `gatsby-plugin-theme-ui`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-plugin-offline`,
       options: {
